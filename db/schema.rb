4739c3a4cf33e4bc154e391e52bf20dc75e3ccc1
Brett Simoens <grmbro@Bretts-MacBook-Pro.local>
2020-02-13 15:28:02 -0500

    adding weather, dont know what happened

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_003918) do
+ActiveRecord::Schema.define(version: 2020_02_13_201904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_02_07_003918) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
